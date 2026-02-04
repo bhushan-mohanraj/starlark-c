ec061fd5712b2d0a5eb0ad09a8b79310da124c06
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2026-02-04 03:15:11 -0800

    Add simple CLI.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main() {
-    printf("Starlark");
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n",
+                argc > 0 ? argv[0] : "starlark");
+        return 1;
+    }
+
+    printf("%s\n", argv[1]);
+
     return 0;
 }
