ef149051ef30e8bfed0f8451423fb28d4cb34aa6
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2026-02-06 01:06:33 -0800

    Fix includes.

--- src/utf8.c
+++ src/utf8.c
@@ -1,6 +1,5 @@
 #include "utf8.h"
 
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
--- src/utf8.h
+++ src/utf8.h
@@ -1,6 +1,8 @@
 #ifndef STARLARK_UTF8_H
 #define STARLARK_UTF8_H
 
+#include <stdint.h>
+
 uint32_t next_code_point(char **stream);
 
 #endif
