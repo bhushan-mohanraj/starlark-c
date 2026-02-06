218e770beed87f86518ef8664432fb372a0fed9d
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2026-02-06 01:02:14 -0800

    Add default filename for `make run`.

--- src/main.c
+++ src/main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
+    char *filename;
+
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <filename>\n",
-                argc > 0 ? argv[0] : "starlark");
-        return 1;
+        filename = "main.star";
+    } else {
+        filename = argv[1];
     }
 
-    printf("%s\n", argv[1]);
+    printf("%s\n", filename);
 
     return 0;
 }
