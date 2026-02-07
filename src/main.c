145877ca37042200142777753d331dbc4f86174f
Bhushan Mohanraj <50306448+bhushan-mohanraj@users.noreply.github.com>
2026-02-06 20:52:20 -0800

    Add `read_file`.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char *read_file(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        return NULL;
+    }
+
+    size_t size = 0;
+    while (fgetc(file) != EOF) {
+        size++;
+    }
+
+    char *content = malloc(size + 1);
+    if (!content) {
+        fclose(file);
+        return NULL;
+    }
+
+    rewind(file);
+    for (size_t i = 0; i < size; i++) {
+        content[i] = fgetc(file);
+    }
+    content[size] = '\0';
+
+    fclose(file);
+    return content;
+}
+
 int main(int argc, char **argv) {
     char *filename;
 
